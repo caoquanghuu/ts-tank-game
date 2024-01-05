9db22bb708480c4da3b0cb3c488a9d4cc94eac09
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-05 15:16:46 +0700

    add: new enum direction replace for old direction

--- src/types.ts
+++ src/types.ts
@@ -3,4 +3,15 @@ type Point = {
   y: number;
 };
 
-export { Point };
+enum Direction {
+  STAND = 0,
+  UP = -1,
+  DOWN = 1,
+  LEFT = -1,
+  RIGHT = 1,
+}
+
+// number;
+// 00001111
+
+export { Point, Direction };
