c705d897a2529ddf2a2a531535ee3a8173a8b9d7
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-05 15:16:12 +0700

    add: new random enum function

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,11 @@
-export function getRandomArbitrary(min:number, max:number) {
+export function getRandomArbitrary(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+}
+
+export const randomEnumValue = (enumeration : any) => {
+  const values = Object.keys(enumeration);
+  const enumKey = values[Math.floor(Math.random() * values.length)];
+  return enumeration[enumKey];
+}
